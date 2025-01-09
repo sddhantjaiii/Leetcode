13b10ade3e96dee769ab9a010c33fa980ba10c7b
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-01-09 22:59:26 +0530

    Time: 9 ms (6.27%) | Memory: 18.3 MB (10.22%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -2,20 +2,14 @@ class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
         j=[]
         c=[]
-        print(nums)
-        
         while len(nums)>0:
             for i in nums:
                 print(i)
                 if i not in j:
-                    j.append(i)
-                    
-                    print(j,c,i)
-                    
+                    j.append(i)                    
             for k in j:
                 nums.remove(k)
             c.append(j)
-            
             j=[]
         return c
             
