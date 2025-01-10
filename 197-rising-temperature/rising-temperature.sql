bd6d2835c7e6c5f775a6b3217917fb2853892f1e
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-01-11 02:26:58 +0530

    Time: 644 ms (44.29%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,19 +1,8 @@
-WITH PreviousWeatherData AS
-(
-    SELECT 
-        id,
-        recordDate,
-        temperature, 
-        LAG(temperature, 1) OVER (ORDER BY recordDate) AS PreviousTemperature,
-        LAG(recordDate, 1) OVER (ORDER BY recordDate) AS PreviousRecordDate
-    FROM 
-        Weather
-)
-SELECT 
-    id 
-FROM 
-    PreviousWeatherData
-WHERE 
-    temperature > PreviousTemperature
-AND 
-    recordDate = DATE_ADD(PreviousRecordDate, INTERVAL 1 DAY);
+SELECT current_day.id
+FROM Weather AS current_day
+WHERE EXISTS (
+    SELECT 1
+    FROM Weather AS yesterday
+    WHERE current_day.temperature > yesterday.temperature
+    AND current_day.recordDate = yesterday.recordDate + INTERVAL 1 DAY
+);
