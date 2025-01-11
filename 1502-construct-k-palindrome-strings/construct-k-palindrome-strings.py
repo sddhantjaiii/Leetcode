2b5347a97bc351d45ee24e9e776b7b3c439283cb
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-01-11 21:38:16 +0530

    Time: 31 ms (71.37%) | Memory: 18.1 MB (34.36%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,19 +1,12 @@
-from collections import Counter
-
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        # If we need more palindromes than the total characters, it's impossible
         if k > len(s):
             return False
 
-        # Count how often each character appears
-        char_count = Counter(s)
-
-        # Count characters that appear an odd number of times
-        odd_count = 0
-        for char, count in char_count.items():
-            if count % 2 == 1:  # If the count is odd
-                odd_count += 1
-
-        # Check if we can form at least k palindromes
-        return odd_count <= k
+        c = Counter(s)
+        print(c)
+        oc = 0
+        for char, count in c.items():
+            if count % 2 == 1:  
+                oc += 1
+        return oc <= k
