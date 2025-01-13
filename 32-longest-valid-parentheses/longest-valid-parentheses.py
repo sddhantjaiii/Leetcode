8e57f8b62c714f928dde5b494ceae4d9a1302734
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-01-13 18:24:45 +0530

    Time: 5 ms (75.44%) | Memory: 18.6 MB (11.36%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        l = [-1]
+        print(l)
+        maxx = 0
+        for i, c in enumerate(s):
+            if c == '(':
+                l.append(i)
+            else:
+                l.pop()
+                if len(l)<1:
+                    l.append(i)
+                else:
+                    maxx = max(maxx, i - l[-1])
+
+        return maxx
