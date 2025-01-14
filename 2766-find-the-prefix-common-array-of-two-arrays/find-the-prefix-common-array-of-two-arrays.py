7359e52cacc99b1aa138e2cf6c8250531877f095
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-01-14 16:30:10 +0530

    Time: 47 ms (18.23%) | Memory: 17.7 MB (45.05%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -3,10 +3,8 @@ class Solution:
         d=[]
         x=0
         for i in range(len(A)):
-            p1=A[:i+1]
-            p2=B[:i+1]
-            p1=set(p1)
-            p2=set(p2)
+            p1=set(A[:i+1])
+            p2=set(B[:i+1])
             for j in p1:
                 if j in p2:
                     x+=1
