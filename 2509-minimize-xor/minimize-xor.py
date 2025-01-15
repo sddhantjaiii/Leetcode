4bc11eb1ed4b8aa121b795e8d1a057abfb6470df
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-01-15 12:09:48 +0530

    Time: 3 ms (18.40%) | Memory: 17.9 MB (14.40%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        b1=list(bin(num1)[2:])
+        b2=bin(num2)[2:]
+        c=Counter(b2)
+        c1=Counter(b1)
+        cc=c["1"]
+        cc1=c1["1"]
+        x=[]
+        print(cc1,cc)
+        for i in range(len(b1)-1,-1,-1):
+            if cc1 > cc:
+                q=cc1-cc
+                if b1[i]=="1":
+                    b1[i]="0"
+                    cc1-=1
+            elif cc>cc1:
+                q=cc-cc1
+                if b1[i]=="0":
+                    b1[i]="1"
+                    cc-=1
+            if cc==cc1:
+                break
+        print(b1,i)
+        while cc>0 and cc!= cc1:
+            x.append("1")
+            cc-=1
+        f=x+b1
+        nn = int(''.join(map(str,f)),2)
+        print(nn)
+        return nn
+        
+
+        
+        
