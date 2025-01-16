0705c499957ed77f21d5a6528e3137d33ed42794
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-01-17 01:03:28 +0530

    Time: 41 ms (14.70%) | Memory: 27.1 MB (7.81%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -7,13 +7,11 @@ class Solution:
         for i in nums:
             p.append(f)
             f*=i
-            
-        
         f=1
         for j in range(u - 1, -1, -1):
             s[j] = f
             f *= nums[j]
-        
         for i in range(u):
             nums[i]=p[i]*s[i]
+        print(p,s)
         return nums
