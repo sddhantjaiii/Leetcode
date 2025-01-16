78ac005c14d548c38de39f334e6f58fce3428b0c
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-01-17 00:08:46 +0530

    Time: 7 ms (39.88%) | Memory: 36.6 MB (27.17%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
@@ -1,25 +1,15 @@
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
-        # Get lengths of arrays
-        len1, len2 = len(nums1), len(nums2)
 
-        # Dictionary to store frequency of each number
-        freq = {}
-
-        # Add frequencies for nums1 elements
-        # Each element appears n2 times in final result
-        for num in nums1:
-            freq[num] = freq.get(num, 0) + len2
-
-        # Add frequencies for nums2 elements
-        # Each element appears n1 times in final result
-        for num in nums2:
-            freq[num] = freq.get(num, 0) + len1
-
-        # XOR numbers that appear odd number of times
+        
+        n1, n2 = len(nums1), len(nums2)
         ans = 0
-        for num in freq:
-            if freq[num] % 2:
-                ans ^= num
-
+        if n2%2==1:
+            for i in range(n1):
+                ans = ans ^ nums1[i]
+        
+        if n1%2==1:
+            for i in range(n2):
+                ans = ans ^ nums2[i]
+        
         return ans
