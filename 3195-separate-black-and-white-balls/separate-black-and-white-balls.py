a491d3448f828901b290c97f9241430cd0ebbfda
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-05-26 02:34:36 +0530

    Time: 63 ms (59.12%) | Memory: 18 MB (49.09%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -1,15 +1,20 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        b=0
-        for i in range(len(s)-1,0,-1):
-            if s[i]=="0":
-                b=i
+        b = -1
+        # Find the rightmost '0'
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                b = i
                 break
-        c=0
-        for i in range(b,-1,-1):
-            
-            if s[i]=="1":
-                c+=b-i
-                b-=1
+
+        if b == -1:
+            return 0  # No zero exists, nothing to do
+
+        c = 0
+        # Move all '1's before position b to the right of zeros
+        for i in range(b - 1, -1, -1):
+            if s[i] == "1":
+                c += b - i
+                b -= 1
+
         return c
-            
