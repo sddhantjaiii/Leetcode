06d19603bc3281d4cee32e71fb7936c325549725
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-06-05 15:45:12 +0530

    Time: 7 ms (32.50%) | Memory: 18 MB (14.72%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,18 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(
-        self, l1: Optional[ListNode], l2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        dummyHead = ListNode(0)
-        curr = dummyHead
-        carry = 0
-        while l1 != None or l2 != None or carry != 0:
-            l1Val = l1.val if l1 else 0
-            l2Val = l2.val if l2 else 0
-            columnSum = l1Val + l2Val + carry
-            carry = columnSum // 10
-            newNode = ListNode(columnSum % 10)
-            curr.next = newNode
-            curr = newNode
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return dummyHead.next
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head=ListNode()
+        res=head
+        t=c=0
+
+        while l1 or l2 or c:
+            t=c
+            if l1:
+                t+=l1.val
+                l1=l1.next
+            if l2:
+                t+=l2.val
+                l2=l2.next
+            num=t%10
+            c=t//10
+            head.next=ListNode(num)
+            head=head.next
+        return res.next
+        
