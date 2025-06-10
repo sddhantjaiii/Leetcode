6a65dbc835521758ba2bfbe8e342fa514ea09e9c
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-06-11 00:14:10 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (76.29%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
@@ -4,8 +4,8 @@ class Solution:
         c=Counter(s)
         odd=0
         even=float('inf')
+        
         for v,key in c.items():
-            print(key)
             if key%2 !=0:
                 odd=max(odd,key)
             else:
