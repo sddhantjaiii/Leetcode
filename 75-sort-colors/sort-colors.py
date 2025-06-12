6a6fbbef56af591ee5141a39feb9eb5839fff4ce
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-06-12 20:38:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.09%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,6 +1,5 @@
 from collections import Counter
 class Solution:
     def sortColors(self, nums):
-        c=Counter(nums)
-        nums[:]=[0]*c[0]+[1]*c[1]+[2]*c[2]
+        return nums.sort()
         
