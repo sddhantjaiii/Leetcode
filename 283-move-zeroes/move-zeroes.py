0de3835c2277272523efc852930defdcc6034ab4
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-06-17 01:24:43 +0530

    Time: 7 ms (44.26%) | Memory: 19.2 MB (9.85%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -6,9 +6,7 @@ class Solution:
             if nums[i] != 0:
                 temp.append(nums[i])
         nt = len(temp)
-        for i in range (0,nt):
-            nums[i] = temp[i]
-        for i in range (nt,n):
-            nums[i]=0
+        nums[:]=temp+[0]*(n-nt)
 
+        return temp
             
