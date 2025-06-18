afff607142625b0cc237108da681eb4b151ee7b8
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-06-19 03:30:11 +0530

    Time: 3 ms (69.60%) | Memory: 18.5 MB (52.82%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -36,12 +36,7 @@ class Solution:
                     continue
             for i in range(1,r):
                 matrix[i][j]=0
-        
-        if matrix[0][0]==0:
-            for i in range(r):
-                matrix[i][0]=0
-            for j in range(c):
-                matrix[0][j]=0
+
         if m0==0:
             for i in range(r):
                 matrix[i][0]=0
