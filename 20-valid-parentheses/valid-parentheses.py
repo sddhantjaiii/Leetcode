ce75a2c4c2c9e51736f06142d965302ac8ea56b9
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-07-02 00:00:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (51.01%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=deque()
+        c=0
+        
+        for i in s:
+            if i=="(" or i=="{" or i=="[":
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                ch=stack.pop()
+                if i==")" and ch=="(" or i=="}" and ch=="{" or i=="]" and ch=="[":
+                    x=1
+                else:
+                    return False
+        if stack:
+            return False
+        return True
+        
