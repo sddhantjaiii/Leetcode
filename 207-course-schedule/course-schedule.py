698ccf60dfa7858e44c9c806a63286ed53f151b5
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-07-05 03:41:06 +0530

    Time: 5 ms (57.88%) | Memory: 18.8 MB (98.55%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,37 @@
+from collections import deque
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj=[[] for __ in range(numCourses)]
+
+        for j in prerequisites:
+            who=j[1]
+            where=j[0]
+            adj[who].append(where)
+
+
+        indegre=[0 for __ in range(numCourses)]
+        for i in range(numCourses):
+            for j in adj[i]:
+                indegre[j]+=1
+
+
+        q=deque()                
+        for i in range(numCourses):
+            if indegre[i]==0:
+                q.append(i)
+        topo=[]
+        while(q):
+            node=q[0]
+            q.popleft()
+            topo.append(node)
+            for i in adj[node]:
+                indegre[i]-=1
+                if indegre[i]==0:
+                    q.append(i)
+        if len(topo)==numCourses:
+            return True
+        
+        return False
+
+
+        
