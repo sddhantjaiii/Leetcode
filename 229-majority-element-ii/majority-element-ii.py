2b1286282febe0cc18738c93f32d0273bed68dd1
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2025-07-31 15:58:26 +0530

    Time: 1 ms (96.56%) | Memory: 22.4 MB (17.51%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -2,9 +2,8 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         from collections import Counter
         c=Counter(nums)
-        l=len(nums)
         r=[]
-        n=l/3
+        n=len(nums)/3
         for i in c:
             if c[i]>n:
                 r.append(i)
