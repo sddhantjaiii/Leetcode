358c115038a34c0d3d9e40283d1f7314942fcfaf
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2025-08-05 13:11:06 +0530

    Time: 12 ms (90.04%) | Memory: 18 MB (39.09%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
@@ -2,10 +2,9 @@ class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
         c=0
         l=len(baskets)
-        r=[0 for __ in range(l)]
         for i in fruits:
             for j in range(l):
-                if i<=baskets[j] and r[j]==0:
+                if i<=baskets[j]:
                     baskets[j]=0
                     c+=1
                     break
