e2eb38a4eb803cf3cce8ee9f8f968cb03a574d18
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2025-09-11 21:11:58 +0530

    Time: 3 ms (4.17%) | Memory: 18.5 MB (22.67%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,41 +1,45 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         m=nums[0]
-        xx=len(nums)**0.5
-        def binarysearch(left,right,m,xx):
+        
+        def binarysearch(left,right,m):
             mid=(left+right)//2
-            xx-=1
+            
             if left==right:
                 return -1
             if nums[mid]>nums[mid+1]:
                 return mid 
             if nums[mid]<nums[mid+1] and nums[mid]>m:
                 m=max(nums[mid+1],m)
-                return binarysearch(mid+1,right,m,xx)
+                return binarysearch(mid+1,right,m)
             else:
                 m=max(nums[mid+1],m)
-                return binarysearch(left,mid,m,xx)
+                return binarysearch(left,mid,m)
 
         if len(nums)==1:
             if nums[0]==target:
                 return 0
             else:
                 return -1
-        x=abs(nums[0]-nums[1])
-        f=binarysearch(0,len(nums)-1,m,xx)
+
+
+        f=binarysearch(0,len(nums)-1,m)
         newarr=nums[f+1:]+nums[:f+1]
         x=len(nums)**(0.5)
         def bs(x,left,right):
             x-=1
             mid=(left+right)//2
-            if x<=-5:
-                return -1
+            print(left,right)
+            
             if target==newarr[mid]:
                 return mid
+            if left==right:
+                return -1
             if newarr[mid]>target:
                 return bs(x,left,mid)
             else:
                 return bs(x,mid+1,right)
+            
         meow=bs(x,0,len(nums)-1)
         if meow==-1:
             return -1
