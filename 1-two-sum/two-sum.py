954a1e1f0720f2ecefdac1dbb4736e0035b31a7e
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2025-09-20 01:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (50.86%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,8 @@
-from typing import List
-
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        lookup = {}
         for i, num in enumerate(nums):
             complement = target - num
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-            num_to_index[num] = i
+            if complement in lookup:
+                return [lookup[complement], i]
+            lookup[num] = i
