f170ae1d64e7639c5d149573a11ca7ea435145c6
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2025-11-07 03:34:09 +0530

    Time: 4 ms (1.47%) | Memory: 18.2 MB (21.85%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,46 +1,30 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        m=nums[0]
+        l,r=0,len(nums)-1
+        while l<r:
+            mid=(l+r)//2
+            if nums[mid]>nums[r]:
+                l=mid+1
+            else:
+                r=mid
+        print(l)
+        x=nums[l:]+nums[:l]
+        print(x)
+        nums=x
+        y=l
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return (mid+y)%(len(nums))
+            elif target<nums[mid]:
+                r=mid-1
+            else:
+                l=mid+1
         
-        def binarysearch(left,right,m):
-            mid=(left+right)//2
-            
-            if left==right:
-                return 0
-            if nums[mid]>nums[mid+1]:
-                return mid +1
-            if nums[mid]<nums[mid+1] and nums[mid]>m:
-                m=max(nums[mid+1],m)
-                return binarysearch(mid+1,right,m)
-            else:
-                m=max(nums[mid+1],m)
-                return binarysearch(left,mid,m)
-
-        if len(nums)==1:
-            if nums[0]==target:
-                return 0
-            else:
-                return -1
+        return -1
 
 
-        f=binarysearch(0,len(nums)-1,m)
-        newarr=nums[f:]+nums[:f]
-        x=len(nums)**(0.5)
-        def bs(left,right):
-            mid=(left+right)//2
+
+
             
-            if target==newarr[mid]:
-                return mid
-            if left==right:
-                return -1
-            if newarr[mid]>target:
-                return bs(left,mid)
-            else:
-                return bs(mid+1,right)
-            
-        meow=bs(0,len(nums)-1)
-        if meow==-1:
-            return -1
-        y=(meow+f+len(nums))%len(nums)
-        
-        return abs(y)
