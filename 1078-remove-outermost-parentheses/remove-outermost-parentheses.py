66d0b5ade849413ca25cd03b77c61f9951478d3c
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2025-11-10 02:35:37 +0530

    Time: 8 ms (9.52%) | Memory: 18.3 MB (16.29%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        from collections import deque
+        stack=deque()
+        x=[]
+        f=[]
+        for i in s:
+            if i=="(":
+                stack.append(i)
+                if len(stack)>1:
+                    x.append(i)
+            else:
+                stack.pop()
+                
+                if len(stack)>0:
+                    x.append(i)
+                if len(stack) == 0:
+                    f.append(x)
+                    x=[]
+        f.append(x)
+        flat_list = [char for sublist in f for char in sublist]
+
+        # Join all elements into a single string
+        result = ''.join(flat_list)
+        return result
+
+
+                            
