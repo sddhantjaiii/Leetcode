3bd37dab47bc5166d8ec8076eaa06529f5639f10
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2025-11-17 02:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.16%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        from collections import deque
+        
+        m=0
+        r=0
+        for i in s:
+            if i == "(":
+                r+=1
+                m=max(m,r)
+            elif i== ")":
+                r-=1
+        return m
+            
+        
