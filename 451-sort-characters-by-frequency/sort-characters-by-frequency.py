1be26d0f0a43f19904870574439d10a21b01eb1d
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2025-11-17 01:53:13 +0530

    Time: 15 ms (29.34%) | Memory: 22.5 MB (15.45%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -2,8 +2,9 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         from collections import Counter
         c=Counter(s)
+        m=max(c.values())
         r=""
-        f=[[] for __ in range(100000)]
+        f=[[] for __ in range(m+1)]
         for key,value in c.items():
             f[value].append(key)
         for i in range(len(f)-1,-1,-1):
