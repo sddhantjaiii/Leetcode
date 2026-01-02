d872f86098b13641bf3c209eab1c57533e956e59
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2026-01-02 15:50:14 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (95.44%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -1,14 +1,8 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        nums.sort()
-        print(nums)
-        c=1
-        prev=nums[0]
-        for i in range(1,len(nums)):
-            if nums[i]==prev :
-                c+=1
+        s=set()
+        for i in nums:
+            if i not in s:
+                s.add(i)
             else:
-                prev=nums[i]
-                c=1
-            if c==len(nums)//2:
-                return nums[i]
+                return i
