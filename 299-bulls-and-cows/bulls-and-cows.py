2f7f6cd8f4963a9d60994195fcee91649f7ab718
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2026-01-21 02:54:44 +0530

    Time: 24 ms (5.41%) | Memory: 19.4 MB (19.17%) - LeetSync

---
+++ 299-bulls-and-cows/bulls-and-cows.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        x=0
+        y=0
+
+        a=[]
+        b=[]
+
+        for i in range(len(secret)):
+            if secret[i]==guess[i]:
+                x+=1
+            else:
+                a.append(secret[i])
+                b.append(guess[i])
+        from collections import Counter
+        a.sort()
+        b.sort()
+        for i in range(len(secret)-x):
+            if a[i] in b:
+                y+=1
+                b.remove(a[i])
+        return str(x)+"A"+str(y)+"B"
