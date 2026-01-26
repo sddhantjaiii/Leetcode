cbcefba49fd842f2a07ea4fefbc585879d47bd29
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2026-01-27 02:30:44 +0530

    Time: 38 ms (85.73%) | Memory: 55.1 MB (58.97%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.ts
@@ -0,0 +1,29 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return{
+        toBe: function(otherVal){
+            if (val===otherVal){
+            return true;
+        } else {
+            throw new Error("Not Equal");
+        }
+    },
+
+    notToBe: function(otherVal){
+        if (val!==otherVal){
+            return true;
+        } else {
+            throw new Error("Equal");
+        }
+    }
+    };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
