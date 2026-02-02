ac1d16587f18aaf2250ddd0d38e93a8ac258cd45
Siddhant Jaiswal <122254271+sddhantjaiii@users.noreply.github.com>
2026-02-02 15:23:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (16.47%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -7,16 +7,13 @@ class Solution:
         c=0
         for i in range(len(digits)-1,-1,-1):
             if digits[i]==9 :
-                print(digits,"up")
                 if i==0:
                     digits[:]=[1]+digits
                     digits[i+1]=0
-                    print("going inside",digits,i)
                     return digits
                 
                 digits[i]=0
                 c=1
-                print(digits,"d")
 
             else:
                 digits[i]+=1
